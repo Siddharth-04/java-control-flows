31463a7637fea60a4c4580e291edb713b39532ed
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-03-03 15:15:09 +0530

    [Siddharth] ADD: Multiplication table of a number java program

---
+++ MultiplicationTableOfNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultiplicationTableOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt(); //input number
+        
+		//priting table
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
