20e43cfa28b820dd393239e49be0afa3722a2342
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-03-03 19:10:06 +0530

    [Siddharth] ADD: Calculator java program

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        double first = scanner.nextDouble(); //first number input
+        
+        System.out.print("Enter an operator : ");
+        String op = scanner.next();  //operator input
+        
+        System.out.print("Enter second number: ");
+        double second = scanner.nextDouble(); //input second number
+        
+        double result;
+        switch (op) {
+            case "+":
+                result = first + second;
+                System.out.println("Result: " + result);
+                break;
+            case "-":
+                result = first - second;
+                System.out.println("Result: " + result);
+                break;
+            case "*":
+                result = first * second;
+                System.out.println("Result: " + result);
+                break;
+            case "/":
+                if (second != 0) {
+                    result = first / second;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
